862d7c4b5237332d3cb34b870a35aa492fe42deb
andy94077 <andy94077@gmail.com>
2019-06-19 23:58:19 +0800

    rename conway_less_mem.py to change1bit.py

--- change1bit.py
+++ change1bit.py
@@ -7,40 +7,21 @@ from joblib import Parallel, delayed
 from multiprocessing import Pool
 
 @jit(nopython=True)
-def over(a,b,n, p=0.5):
-    ans = 0.0
-    now_p=1.0
+def over(a,b,n):
+    ans = 0
     for i in range(n-1,-1,-1):
-        #ans <<= 1
-        ans += int(a ^ b == 0)*now_p
-        now_p *= p if (b&1) == 1 else 1-p
+        ans <<= 1
+        ans += int(a ^ b == 0)
         a &= (1 << i) - 1
         b >>= 1
     return ans
-
-@jit(nopython=True)
-def init(n):
-    total = 1 << n
-    rate = np.full((total, total),0.5)
-    for i in range(total):
-        for j in range(total):
-            if i != j:
-                BB, BA, AB, AA = over(j, j,n), over(j, i,n), over(i, j,n), over(i, i,n)
-                rate[i,j]=(BB - BA) / (BB - BA - AB + AA)
-    #rate = np.array([init_multi(i, j, n) for i in range(total) for j in range(total)]).reshape(total, total)
-    return rate
 
 def multi(rate, n, i):
     total = 1 << n
     subpro=rate[[i^(1<<j) for j in range(n)]+[i]]
 
     A_choices = np.argmax(subpro, axis=0)
-    '''
-    if i == 4:
-        print([i^(1<<j) for j in range(n)]+[i],subpro,sep='\n')#print(list(map(bin,list(i^(1<<A_choices))+[i])))'''
     subpro = np.max(subpro,axis=0)
-    '''if i==4:
-        print(subpro)'''
     B_choice = np.argmin(subpro)
     A_choice2 = i^(1<<A_choices[B_choice]) if A_choices[B_choice]<n else i
     # A, B, A, rate
@@ -86,36 +67,13 @@ def change1bit_less_mem(n):
 
 def main(n):
     total = 1<<n
-    #rate = init(n)
-
-    '''
-    print('basic:')
-    max_val_index = np.min(rate,axis=1)==np.max(np.min(rate,axis=1))
-    for i in np.arange(rate.shape[0])[max_val_index]:
-        min_index = np.argmin(rate[i])
-        print('A=' + bin(i)[2:].zfill(n), 'B=' + bin(min_index)[2:].zfill(n), 'rate=' + str(rate[i,min_index]))
-    for i in np.arange(rate.shape[0])[~max_val_index]:
-        min_index = np.argmin(rate[i])
-        print('A=' + bin(i)[2:].zfill(n), 'B=' + bin(min_index)[2:].zfill(n), 'rate=' + str(rate[i,min_index]))
-    '''
-    '''
-    print(' '*n, *[' %*s'%(max(n,5), bin(i)[2:].zfill(n)) for i in range(total)])
-    for i,row in enumerate(rate):
-        print(bin(i)[2:].zfill(n),end=' ')
-        for item in row:
-            print(' %*.3f' % (max(n,5),item), end=' ')
-        print()
-    '''
     print('change 1 bit:')
-    #ch1 = change1bit(rate, n)
     ch1 = change1bit_less_mem(n)
     max_index = np.argmax(ch1[:, 3])
     
-    for item in ch1[max_index:max_index+1]:
-        print('A=',bin(int(item[0]))[2:].zfill(n),' B=',bin(int(item[1]))[2:].zfill(n),' A=',bin(int(item[2]))[2:].zfill(n),' rate=',item[3],sep='')
-    '''max_index = np.max(ch1[:, 3])
+    max_index = np.max(ch1[:, 3])
     for item in ch1[ch1[:, 3] == max_index]:
-        print('A=',bin(int(item[0]))[2:].zfill(n),' B=',bin(int(item[1]))[2:].zfill(n),' A=',bin(int(item[2]))[2:].zfill(n),' rate=',item[3],sep='')'''
+        print('A=',bin(int(item[0]))[2:].zfill(n),' B=',bin(int(item[1]))[2:].zfill(n),' A=',bin(int(item[2]))[2:].zfill(n),' rate=',item[3],sep='')
     
     '''
     for item in ch1[ch1[:, 3] != max_index]:
